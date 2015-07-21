de2fee06cdc471035b48087004b3fa359c899f2f
lewis617 <897473127@qq.com>
2015-07-21 22:55:14 +0800

    复制进来两个例子，bootstrap和上传图片

---
+++ uploadImage/uploadImage.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+from flask import Flask, render_template, request,url_for
+import os, sys
+
+app = Flask(__name__)
+
+image_path = os.path.join(sys.path[0], 'static')
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    image_path = os.path.join(sys.path[0], 'static')
+    image = request.files['file']
+    image.save(os.path.join(image_path, image.filename))
+    imgHtml = '<img src="' + url_for('static', filename= image.filename) + '"/>'
+    return imgHtml
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=80)
