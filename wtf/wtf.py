e52f1cbadab408fa9842f2bc6b804e526bab7722
lewis617 <897473127@qq.com>
2015-07-27 22:43:05 +0800

    wtf练手，注意SECRET_KEY的拼写，错误提示通过form.error.字段名显示

---
+++ wtf/wtf.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template,redirect
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+bootstrap = Bootstrap(app)
+
+
+class NameForm(Form):
+    name = StringField("what's your name?", validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
+
+
+if __name__ == '__main__':
+    app.run()
