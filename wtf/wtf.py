23f9214064c4e6dff7de40a273203f16d072dc0f
lewis617 <897473127@qq.com>
2015-07-29 14:26:10 +0800

    wtf中CSRF测试

--- wtf/wtf.py
+++ wtf/wtf.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,redirect
+from flask import Flask, render_template,request
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
@@ -21,8 +21,12 @@ def index():
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+    return render_template('index.html',form=form,name=name)
 
+@app.route('/load/', methods=['GET', 'POST'])
+def load():
+    print(request.form["name"])
+    return "<div>617</div>"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8001)
