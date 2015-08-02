72f5264a73d16264ff49e131b27b51996be13fb5
lewis617 <897473127@qq.com>
2015-08-02 08:42:54 +0800

    jinja测试

---
+++ jinjaTest/jinjaTest.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+from flask.ext.bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap= Bootstrap(app)
+
+
+@app.route('/')
+def hello_world():
+    name=u'name';
+    print name
+    user_info={u'name':u"lewis",u"age":u"23",u"sex":u"male"}
+    return render_template("index.html",user_info=user_info)
+
+
+if __name__ == '__main__':
+    app.run()
