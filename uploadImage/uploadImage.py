fb6fefec160144eaaefa0561353f2822bf1fab5a
lewis617 <897473127@qq.com>
2015-09-06 15:34:00 +0800

    html5上传带进度条

--- uploadImage/uploadImage.py
+++ uploadImage/uploadImage.py
@@ -9,18 +9,18 @@ image_path = os.path.join(sys.path[0], 'static')
 
 @app.route('/')
 def hello():
-    return render_template('index1.html')
+    return render_template('html5_upload.html')
 
 
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload/', methods=['POST'])
 def upload_file():
     image_path = os.path.join(sys.path[0], 'static')
-    image = request.files['file']
+    image = request.files['fileToUpload']
     image.save(os.path.join(image_path, image.filename))
     imgHtml = '<img src="' + url_for('static', filename=image.filename) + '"/>'
     return imgHtml
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
