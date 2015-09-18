d35af95a2369a1cc81a4340edc601b66b6483702
lewis617 <lewis9921006>
2015-09-18 20:33:39 +0800

    如果想使用js获取页面上的数据，必须用json.dumps，如果里面的中文显示unicode乱码，可以使用ensure_ascii=False来转换成中文

--- load&dump/load&dump.py
+++ load&dump/load&dump.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from flask import Flask, render_template
 import json
 
@@ -6,7 +7,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    json_text = '{"name":"刘一奇","age":"12","sex":"男"}'
+    json_dic = {"name": "刘一奇", "age": "12", "sex": "男"}
+    json_str = json.dumps(json_dic)
+    return render_template("index.html", json_str=json_str)
 
 
 @app.route('/getstr/', methods=["GET"])
@@ -15,12 +19,14 @@ def hello_getstr():
     json_str = json.dumps(dic)
     return json_str
 
+
 @app.route('/getjson/', methods=["GET"])
 def hello_getjson():
     json_text = '{"name":"lewis","age":"12","sex":"man"}'
     json_dic = json.loads(json_text)
-    json_str=json.dumps(json_dic)
+    json_str = json.dumps(json_dic)
     return json_str
+
 
 if __name__ == '__main__':
     app.run(debug="true")
