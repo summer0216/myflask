91ca5c0ae703070f4fa31273f77d35c37353103c
lewis617 <897473127@qq.com>
2016-01-06 10:51:03 +0800

    bug fix

--- oss-flask-upload/oss/get_bucket.py
+++ oss-flask-upload/oss/get_bucket.py
@@ -11,17 +11,23 @@ bucket = oss2.Bucket(auth, endpoint, 'jingyun-upload')
 
 files = []
 
+for obj in oss2.ObjectIterator(bucket, delimiter='/',prefix='JingYun/kong/'):
+    if obj.is_prefix():  # 文件夹
+        print('directory: ' + obj.key)
+    else:                # 文件
+        print('file: ' + obj.key)
 
 def digui(pre, parent):
     for obj in oss2.ObjectIterator(bucket, delimiter='/', prefix=pre):
-        array=obj.key.split("/")
-        if array[-1] is "":
-            parent.append({"text": array[-2], "children": []})
-        else:
-            parent.append({"text": array[-1], "children": []})
+        array = obj.key.split("/")
         if obj.is_prefix():  # 文件夹
+            parent.append({"text": array[-2], "children": []})
             digui(obj.key, parent[-1]["children"])
+        else:
+            if array[-1] is not "":
+                parent.append({"text": array[-1], "children": []})
 
 
-digui('',files)
+
+digui('JingYun/', files)
 print(files)
